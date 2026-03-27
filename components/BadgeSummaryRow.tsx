b1c148d7d3e9376fe61bb372d3a59fb36961de90
adamjpwestsr-ctrl <adamjpwestsr@gmail.com>
2026-03-27 15:55:41 -0400

    Update BadgeSummaryRow.tsx

--- components/BadgeSummaryRow.tsx
+++ components/BadgeSummaryRow.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import BadgeHoverCard from "../BadgeHoverCard";
+import BadgeHoverCard from "./BadgeHoverCard";
 import { getBadgeTier } from "./BadgeTier";
 
 type BadgeSummaryRowProps = {

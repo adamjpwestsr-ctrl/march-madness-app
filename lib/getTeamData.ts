2376d45673304540f7bf4a244c3c7174719dc48b
adamjpwestsr-ctrl <adamjpwestsr@gmail.com>
2026-03-28 00:04:42 -0400

    Update getTeamData.ts

--- lib/getTeamData.ts
+++ lib/getTeamData.ts
@@ -1,4 +1,8 @@
-import teams from '../data/teams.json'
+import rawTeams from '../data/teams.json'
+import { TeamData } from '../types/types'
+
+// Tell TypeScript this JSON is a dictionary keyed by string
+const teams = rawTeams as Record<string, TeamData>
 
 export function getTeamData(teamName: string) {
   if (!teamName) return null

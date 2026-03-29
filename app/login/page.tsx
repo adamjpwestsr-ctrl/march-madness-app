846dc3e345c02d9a08b88e3ddfdd154657c12c16
adamjpwestsr-ctrl <adamjpwestsr@gmail.com>
2026-03-29 13:03:15 -0400

    Create page.tsx
    login page creation

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from './LoginForm';
+
+export default function LoginPage() {
+  return (
+    <div style={{ maxWidth: 400, margin: '80px auto', padding: 20 }}>
+      <LoginForm />
+    </div>
+  );
+}

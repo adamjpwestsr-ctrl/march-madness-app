6e56ae00012df28b2854d1011948fd35bc22c290
Adam West <adamjpwestsr@gmail.com>
2026-03-30 01:36:02 -0400

    Include floating logos on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,20 @@
 import LoginForm from './LoginForm';
+import styles from './login.module.css';
 
 export default function LoginPage() {
   return (
-    <div style={{ maxWidth: 400, margin: '80px auto', padding: 20 }}>
-      <LoginForm />
+    <div className={styles.pageWrapper}>
+      <div className={styles.floatingLogos}>
+        <img src="/logos/uconn.png" className={styles.logo1} />
+        <img src="/logos/duke.png" className={styles.logo2} />
+        <img src="/logos/unc.png" className={styles.logo3} />
+        <img src="/logos/kansas.png" className={styles.logo4} />
+        <img src="/logos/arizona.png" className={styles.logo5} />
+      </div>
+
+      <div className={styles.formWrapper}>
+        <LoginForm />
+      </div>
     </div>
   );
 }

bbd48c0ae636d89a8c6f9fc29a7653d467df42a8
Adam West <adamjpwestsr@gmail.com>
2026-05-02 13:18:08 -0400

    Auth Guard + Leaderboard Architecture Foundation + Clean up Components

--- app/_legacy/leaderboard/page.tsx
+++ app/_legacy/leaderboard/page.tsx
@@ -4,7 +4,7 @@ import type React from 'react'
 import { useEffect, useState } from 'react'
 import { supabase } from "lib/supabaseClient";
 import { getTeamLogo } from 'lib/getTeamLogo'
-import HistoryOverlay from '@/app/leaderboard/HistoryOverlay'
+import HistoryOverlay from '@/app/_legacy/leaderboard/HistoryOverlay'
 import TeamHoverCard from '../../components/TeamHoverCard'
 import BadgeStrip from '@/components/BadgeStrip'
 import ForumPanel from './ForumPanel'

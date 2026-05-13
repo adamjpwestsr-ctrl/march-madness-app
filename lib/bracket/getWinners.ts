eb31ff317e0f8413930e5f502491092001b4cad1
Adam West <adamjpwestsr@gmail.com>
2026-05-13 00:38:15 -0400

    v7.0.0 - Implementation of full updated bracket for the March Madness Expansion ahead of 2026-2027 Season

--- lib/bracket/getWinners.ts
+++ lib/bracket/getWinners.ts
@@ -1,6 +1,9 @@
 import type { Game, Team } from './types';
 
-export function getWinners(openingRoundGames: Game[], teams: Team[]): Team[] {
+export function getWinners(
+  openingRoundGames: Game[],
+  teams: Team[]
+): (Team | null)[] {
   return openingRoundGames.map(game => {
     if (!game.winner_id) return null;
     return teams.find(t => t.id === game.winner_id) ?? null;

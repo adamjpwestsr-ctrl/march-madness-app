9937ddba3f01ebfc4ac76821d93440f30a165317
Adam West <adamjpwestsr@gmail.com>
2026-06-17 15:43:18 -0400

    v17.0.8 - Fixing home page display name

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: false,
-  },
+  // Remove the experimental turbo flag entirely
+  experimental: {},
+
+  // Force Node.js runtime globally (fixes useSearchParams + prerender errors)
+  runtime: "nodejs",
+
+  // Disable static generation globally unless explicitly enabled
+  dynamic: "force-dynamic",
 };
 
 module.exports = nextConfig;

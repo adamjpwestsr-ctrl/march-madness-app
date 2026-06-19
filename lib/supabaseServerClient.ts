7f63077a8f1a6f40b74aa2da1a92c6f5aa7b2b73
Adam West <adamjpwestsr@gmail.com>
2026-06-19 13:40:00 -0400

    v17.1.0 - Resolving issue on Golf Weekly where picks are all saving to one profile

--- lib/supabaseServerClient.ts
+++ lib/supabaseServerClient.ts
@@ -2,21 +2,12 @@
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
-export function createSupabaseServerClient() {
-  const cookieStore = cookies();
-
+export function supabaseServerClient() {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
-        getAll: () => cookieStore.getAll(),
-        setAll: (cookiesToSet) => {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookieStore.set(name, value, options)
-          );
-        },
-      },
+      cookies,
     }
   );
 }

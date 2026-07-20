01c5bf1b61c504ace326651551f4290ff6fe6445
Adam West <adamjpwestsr@gmail.com>
2026-07-20 00:55:22 -0400

    v25.1.1 - Resolve error in the Trivia challenge. Questions not available

---
+++ app/trivia/page.tsx
@@ -0,0 +1,16 @@
+export default function YouAreHere() {
+  return (
+    <div
+      style={{
+        padding: "40px",
+        fontSize: "48px",
+        fontWeight: "bold",
+        textAlign: "center",
+        background: "hotpink",
+        color: "black",
+      }}
+    >
+      YOU ARE HERE
+    </div>
+  );
+}

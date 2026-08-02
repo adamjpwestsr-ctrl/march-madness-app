83da7f48f1e337bca7ac30e76c67cdf0d607feb2
Adam West <adamjpwestsr@gmail.com>
2026-08-01 23:09:41 -0400

    v25.3.2 - Review Login Loading Page

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+write "I AM HERE"
+
 import { useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
 import { loginWithEmail, verifyAdminCode } from "./actions";

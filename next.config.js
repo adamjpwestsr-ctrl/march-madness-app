da0cf449ff0c4df7cb458d13607d751a799dccf2
Adam West <adamjpwestsr@gmail.com>
2026-08-04 14:57:43 -0400

    v25.3.2 - Review Login Loading Page

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Remove the experimental turbo flag entirely
-  experimental: {},
-
-  // Force Node.js runtime globally (fixes useSearchParams + prerender errors)
-  runtime: "nodejs",
-
-  // Disable static generation globally unless explicitly enabled
-  dynamic: "force-dynamic",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;

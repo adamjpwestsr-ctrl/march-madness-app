0ab181fee961d9426fcd96473994ec66542f9a9c
Adam West <adamjpwestsr@gmail.com>
2026-08-06 13:50:17 -0400

    v25.3.2 - Review Login Loading Page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { LogOut } from "lucide-react";
 
-export default async function Header() {
+export default function Header() {
   const router = useRouter();
 
   const handleLogout = async () => {
